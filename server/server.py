526a8ee2961a09266e881221cbd619439333f0f5
yarivtch <yarivtch@gmail.com>
2024-11-30 23:20:25 +0200

    add port to build

--- server/server.py
+++ server/server.py
@@ -315,4 +315,5 @@ preload_model()
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5002))
+    logger.info(f"Starting server on port {port}")
     app.run(host='0.0.0.0', port=port, debug=False)
